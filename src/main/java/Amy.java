5fa103e6d43a119752ef2954ca2913ce1913f78c
Amritaa <amritaavarsni@gmail.com>
2026-08-21 12:44:47 +0800

    Rename Duke to Amy

---
+++ src/main/java/Amy.java
@@ -0,0 +1,10 @@
+public class Amy {
+    public static void main(String[] args) {
+        String banner = "  A     m   m  y   y\n"
+                + " A A    mm mm   y y\n"
+                + "AAAAA   m m m    y\n"
+                + "A   A   m   m    y\n"
+                + "A   A   m   m    y\n";
+        System.out.println(banner);
+    }
+}
