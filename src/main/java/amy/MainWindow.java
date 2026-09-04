3a187c43921e43b059f738a3dc22740b2683d5f5
Amritaa <amritaavarsni@gmail.com>
2026-09-04 21:38:59 +0800

    JavaFX tutorial 4, Refactor Amy to use FXML

---
+++ src/main/java/amy/MainWindow.java
@@ -0,0 +1,52 @@
+package amy;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.VBox;
+/**
+ * Controller for the main GUI.
+ */
+
+public class MainWindow extends AnchorPane {
+    @FXML
+    private ScrollPane scrollPane;
+    @FXML
+    private VBox dialogContainer;
+    @FXML
+    private TextField userInput;
+    @FXML
+    private Button sendButton;
+
+    private Amy amy;
+
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/img.png"));
+    private Image amyImage = new Image(this.getClass().getResourceAsStream("/images/img_1.png"));
+
+    @FXML
+    public void initialize() {
+        scrollPane.vvalueProperty().bind(dialogContainer.heightProperty());
+    }
+
+    /** Injects the Duke instance */
+    public void setAmy(Amy a) {
+        amy = a;
+    }
+
+    /**
+     * Creates two dialog boxes, one echoing user input and the other containing Duke's reply and then appends them to
+     * the dialog container. Clears the user input after processing.
+     */
+    @FXML
+    private void handleUserInput() {
+        String input = userInput.getText();
+        String response = amy.getResponse(input);
+        dialogContainer.getChildren().addAll(
+                DialogBox.getUserDialog(input, userImage),
+                DialogBox.getDukeDialog(response, amyImage)
+        );
+        userInput.clear();
+    }
+}
